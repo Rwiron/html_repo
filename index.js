48001edc2d5aac6ee041f62bfa2a580f49960a1e
Rwiron <ruzindanawiron@gmail.com>
2024-02-12 11:47:21 +0200

    validation

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-console.log(document.getElementById('intro'));
+
+function Login() {
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+    let valid = true;
+    
+    if (email === '') {
+        document.getElementById('emailError').innerHTML = 'Please Enter your Email ';
+        valid = false;
+    } else {
+        document.getElementById('emailError').innerHTML = '';
+    }
+
+    if (password === '') {
+        document.getElementById('passwordError').innerHTML = 'Password is required';
+        valid = false;
+    } else {
+        document.getElementById('passwordError').innerHTML = '';
+    }
+
+    if (valid) {
+        console.log('Email:', email);
+        console.log('Password:', password);
+    }
+}
